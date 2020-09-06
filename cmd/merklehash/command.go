480b220be46f22bbd1a487b5aa0bdecb25908535
Chris Doherty <chris.doherty@alteryx.com>
2020-09-06 16:45:34 -0600

    Use new algorithm in cli prog

--- cmd/merklehash/command.go
+++ cmd/merklehash/command.go
@@ -1,55 +1,110 @@
 package main
 
 import (
+	"context"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 
-	"github.com/chrisdoherty4/merklehash/pkg/merkletree"
+	"github.com/chrisdoherty4/merklehash/merkletree"
 	"github.com/spf13/cobra"
 )
 
+var ()
+
 var (
-	algorithm string
+	// The long help for the command
+	longHelp string
+
+	// A map of supported algorithms.
+	algs map[string]merkletree.HashFactory
 
 	// MerkleHashCmd represents the root command for the merkle hasher.
-	MerkleHashCmd = &cobra.Command{
-		Use:   "merklehash <directory path>",
-		Short: "MerkleHash is a directory hasher.",
-		Long: `MerkleHash is a hashing tool for generating digests of arbitrary depth 
-directory hierarchies.
+	merkleHashCmd *cobra.Command
 
-MerkleHash will output the hash of the directory followed by the directory that 
-was hashed.`,
-		Example: "  merklehash /path/to/directory",
-		Args:    cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			if !merkletree.Supports(algorithm) {
-				fmt.Fprintf(os.Stdout, "'%v' is not a valid algorithm. Value algorithms are:\n", algorithm)
-				for _, ident := range merkletree.GetAlgorithms() {
-					fmt.Printf("  %v\n", ident)
-				}
-				os.Exit(1)
-			}
-
-			fmt.Printf(
-				"%x\n",
-				merkletree.New(args[0], merkletree.GetHasher(algorithm)).Hash(),
-			)
-		},
-	}
-
-	supportsCmd = &cobra.Command{
-		Use:   "algorithms",
-		Short: "List supported algorithms.",
-		Run: func(cmd *cobra.Command, args []string) {
-			for _, ident := range merkletree.GetAlgorithms() {
-				fmt.Printf("%v\n", ident)
-			}
-		},
-	}
+	// Optional argument
+	algorithm string
 )
 
 func init() {
-	MerkleHashCmd.Flags().StringVarP(&algorithm, "algorithm", "a", "sha256", "Hashing algorithm to use")
-	MerkleHashCmd.AddCommand(supportsCmd)
+	algs = make(map[string]merkletree.HashFactory)
+	algs = map[string]merkletree.HashFactory{
+		"md5":        md5.New,
+		"sha1":       sha1.New,
+		"sha224":     sha256.New224,
+		"sha256":     sha256.New,
+		"sha384":     sha512.New384,
+		"sha512":     sha512.New,
+		"sha512/224": sha512.New512_224,
+		"sha512/256": sha512.New512_256,
+	}
+
+	algsSlice := []string{}
+	for alg := range algs {
+		algsSlice = append(algsSlice, alg)
+	}
+
+	sort.Strings(algsSlice)
+	algsSlice = mapSlice(algsSlice, func(v string) string {
+		return fmt.Sprintf("  * %v", v)
+	})
+
+	longHelp = fmt.Sprintf(`
+merklehash is a hashing tool for generating digests of arbitrary depth directory 
+hierarchies.
+
+Supported algorithms include:
+%v
+`,
+		strings.Join(algsSlice, "\n"),
+	)
+
+	merkleHashCmd = &cobra.Command{
+		Use:     "merklehash <directory path>",
+		Short:   "merklehash is a directory hasher.",
+		Long:    longHelp,
+		Example: "  merklehash /path/to/directory",
+		Args:    cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			factory, ok := algs[algorithm]
+
+			if !ok {
+				fmt.Fprintf(os.Stderr, "Unsupported algorithm %v", algorithm)
+				os.Exit(1)
+			}
+
+			hash, err := merkletree.New(
+				context.Background(),
+				args[0],
+				factory,
+			)
+
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+
+			fmt.Printf("%x\n", hash)
+		},
+	}
+
+	merkleHashCmd.Flags().StringVarP(
+		&algorithm,
+		"algorithm",
+		"a",
+		"sha256",
+		"hashing algorithm",
+	)
+}
+
+func mapSlice(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
 }
--- cmd/merklehash/main.go
+++ cmd/merklehash/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	if err := MerkleHashCmd.Execute(); err != nil {
+	if err := merkleHashCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
